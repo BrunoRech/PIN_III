9e7716a0c23823e4c67481351b732e6b833f0342
Bruno Rech <brunogrech@hotmail.com>
2019-08-27 19:48:27 -0300

    ajustes nas bibliotecas jwt

---
+++ backend/src/controllers/LoginController.js
@@ -0,0 +1,24 @@
+const JwtController = require('./JwtController');
+
+module.exports = {
+
+    login(req, res) {
+        if (req.body.email === 'teste' && req.body.password === '123') {
+            //auth ok
+            const token = JwtController.generateToken();
+            return res.status(200).send({ auth: true, token: token });
+        }
+        return res.status(500).send('Login inválido!');
+
+    },
+
+    teste(req,res){
+        const token = req.headers['token'];
+        const response = JwtController.verifyToken(token, res);
+        //console.log(reponse);
+       // return res.send(response);
+       return;
+
+    }
+}
+
