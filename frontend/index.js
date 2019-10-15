14149a934b29f8a9cf72abe679b80c1ffc98c871
jefip <jefipnz@gmail.com>
2019-10-14 21:07:49 -0300

    Ajustes de proxy.

--- frontend/index.js
+++ frontend/index.js
@@ -1,5 +1,8 @@
-const express = require('express'); 
+const express = require('express');
 const app = express();
+const http = require('http');
+
+// yarn dev 
 
 const webpack = require('webpack');
 const webpackDevMiddleware = require('webpack-dev-middleware');
@@ -17,5 +20,37 @@ app.use(webpackDevMiddleware(compiler, {
 app.use(webpackHotMiddleware(compiler));
 
 app.use(express.static(__dirname));
+
+app.all('/api/*', function (req, res) {
+    var creq = http.request({
+        host: req.headers.location,
+        port: 3000,
+        path: req.url,
+        method: req.method,
+        headers: req.headers
+    }, function (cres) {
+        cres.setEncoding('utf8');
+        let sData = '';
+        cres.on('data', function (chunk) {
+            sData += chunk;
+        });
+        cres.on('close', function () {
+            res.writeHead(cres.statusCode);
+            res.write(sData);
+            res.end();
+        });
+        cres.on('end', function () {
+            res.writeHead(cres.statusCode);
+            res.write(sData);
+            res.end();
+        });
+    }).on('error', function (e) {
+        console.log(e.message);
+        res.writeHead(500);
+        res.end();
+    });
+
+    creq.end();
+});
 
 app.listen(80);
