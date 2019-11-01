097f119ca14c8a59a1f48694e9d65e72f9a52524
brunorech <brunogrech@hotmail.com>
2019-10-31 22:15:08 -0300

    Arrumando o consumo da api da udemy

--- backend/src/controllers/Udemy/UdemyController.js
+++ backend/src/controllers/Udemy/UdemyController.js
@@ -3,27 +3,55 @@ const request = require('request');
 
 const user_id = 'VT7KEMNDeGeKszExmxsVUarAK9jS7zKz2D3gZQ0r';
 const client_secret = 'nidauKZKZQ7IZQCkXZDi3Vho87Tu4lzOxHZLbunpU08xBIud7ZPQx0NEnzI81LHRbJMen6JG371GzTcNxbPTjV9zRWLVLES0rSHV3Rw7wWtxJ6TbzMvOQU0GMRhHdLlb'
+const baseUdemyUrl = 'https://www.udemy.com';
+const coursesUrl = 'https://www.udemy.com/api-2.0/courses/';
 
-const url = 'https://www.udemy.com/api-2.0/courses';
-
-const options = {
-    url,
-    auth: {
-        user: user_id,
-        password: client_secret
+const getRating = async id =>{
+    const options = {
+        url: coursesUrl+id+'/reviews/',
+        auth: {
+            user: user_id,
+            password: client_secret
+        }
     }
+    await request(options, (err, res, body) => {
+        const {results} = JSON.parse(body);
+        let summ = 0;
+
+        results.map(r =>{
+            summ += parseInt(r.rating);
+        });
+
+        return summ/results.length;
+
+    });
 }
 
 module.exports = {
-    async teste(req, res) {
-        let bodyResponse;
-        request(options, (err, res, body) => {
+    async teste(req, resposta) {
+        const options = {
+            url: coursesUrl,
+            auth: {
+                user: user_id,
+                password: client_secret
+            }
+        }
+
+        await request(options, (err, res, body) => {
             if (err) {
                 return console.log(err);
             }
-            bodyResponse = body;
+            const {results} = JSON.parse(body);
+            const arr = [];
+            results.map(async r => {
+                const {title, url, image_480x270, description, primary_category, id } = r
+                const {amount} = r.price_detail;
+                const rating = await getRating(id);
+                
+                return arr.push({title, url:baseUdemyUrl+url, amount, image_480x270, description, primary_category, rating});
+            });
+             return resposta.json(arr);
         });
-        return res.status(200).json(bodyResponse);
     },
 
     async getUserById(req, res) {
